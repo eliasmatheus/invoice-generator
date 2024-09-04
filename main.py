4beb7f112d2a9ed518057a3f2ab4ac908d360610
Elias Matheus Oliveira <elias.matheus@rainforest.tech>
2024-09-04 10:17:55 -0300

    chore: Update .gitignore to ignore .DS_Store file

--- main.py
+++ main.py
@@ -12,37 +12,43 @@ from models.sender import Sender
 
 
 # Modifique apenas abaixo dessa linha
-invoice = Invoice('019', 2023, 'August', 28, 'U$ 2,166.67')
+invoice = Invoice("24-008", 2024, "Aug", 30, "U$ 2,166.67")
 sender = Sender(
-    'Elias Matheus Melo de Oliveira',
-    'eliasmatheus@hotmail.com',
-    'Founder',
-    'OLITECH LTDA',
-    '+55 21 99988-7172')
-recipient = Contact('Payments Rainforest', 'payment@rainforest.tech')
+    "Elias Matheus Melo de Oliveira",
+    "eliasmatheus@hotmail.com",
+    "Founder",
+    "OLITECH LTDA",
+    "+55 21 99988-7172",
+)
+recipient = Contact("Payments Rainforest", "payment@rainforest.tech")
 
 # Modifique apenas acima dessa linha
 
 
-results_path = 'result/invoice'
+results_path = "result/invoice"
 
 # usage
-doc = Document('./template/template.docx')
-docx_replace(doc, dict(
-    InvoiceNumber=invoice.number,
-    InvoiceDate=invoice.date,
-    InvoiceMonth=invoice.month,
-    InvoiceValue=invoice.value,))
-doc.save(f'{results_path}-{invoice.number}-{invoice.year}.docx')
+doc = Document("./template/template.docx")
+docx_replace(
+    doc,
+    dict(
+        InvoiceNumber=invoice.number,
+        InvoiceDate=invoice.date,
+        InvoiceMonth=invoice.month,
+        InvoiceValue=invoice.value,
+    ),
+)
+doc.save(f"{results_path}-{invoice.number}-{invoice.year}.docx")
 
 # convert to PDF
 convert(
     f"{results_path}-{invoice.number}-{invoice.year}.docx",
-    f"{results_path}-{invoice.number}-{invoice.year}.pdf")
+    f"{results_path}-{invoice.number}-{invoice.year}.pdf",
+)
 
-outlook = app('Microsoft Outlook')
+outlook = app("Microsoft Outlook")
 
-p = Path(f'{results_path}-{invoice.number}-{invoice.year}.pdf')
+p = Path(f"{results_path}-{invoice.number}-{invoice.year}.pdf")
 p = Alias(str(p))
 
 
@@ -54,22 +60,17 @@ subject = outgoing_message.get_subject()
 
 msg = outlook.make(
     new=k.outgoing_message,
-    with_properties={
-        k.subject: subject,
-        k.content: content + signature})
+    with_properties={k.subject: subject, k.content: content + signature},
+)
 
 msg.make(
     new=k.recipient,
     with_properties={
-        k.email_address: {
-            k.name: recipient.name,
-            k.address: recipient.email}})
-
-msg.make(
-    new=k.attachment,
-    with_properties={
-        k.file: p}
+        k.email_address: {k.name: recipient.name, k.address: recipient.email}
+    },
 )
+
+msg.make(new=k.attachment, with_properties={k.file: p})
 
 msg.open()
 msg.activate()
